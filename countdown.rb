b51b70f493d9ba948657a9dc9af09c724440251f
Jorge Ramirez <joramirez96@gmail.com>
2020-03-26 21:54:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,17 @@
 #write your code here
 def countdown(num)
-  while num > 5
+  return "HAPPY NEW YEAR!"
+  while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
-    return "HAPPY NEW YEAR!"
   end
 end
 
 def countdown_with_sleep(num)
-  while num > 5
+  return "HAPPY NEW YEAR!"
+  while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
     sleep 1.seconds
-    return "HAPPY NEW YEAR!"
-  end
+    end
 end
