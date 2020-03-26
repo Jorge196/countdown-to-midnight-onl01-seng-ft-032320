dbd76e909bb5bb1d2e3924abb9bc17cc0cba9041
Jorge Ramirez <joramirez96@gmail.com>
2020-03-26 21:45:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
-def countdown
-  x= 10
-  while x > 0
-    puts "#{x} SECONDS!"
-    x -= 0
-    puts  "HAPPY NEW YEAR"
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    return "HAPPY NEW YEAR!"
   end
 end
