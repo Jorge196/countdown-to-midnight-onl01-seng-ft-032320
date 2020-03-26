dd2d0447bc34c01c83c681b6ca7f0bf63a9170e8
Jorge Ramirez <joramirez96@gmail.com>
2020-03-26 21:39:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
-
-def countdown
+x = 0 
+def countdown(number) 
+  while number > x 
+    put "#{number} SECOND(S)!"
+    number -= 1
+    return "HAPPY NEW YEAR"
+  end
+end
+ 
+def countdown_with_sleep
 end
